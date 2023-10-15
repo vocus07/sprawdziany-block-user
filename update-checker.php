ba30fef5a2381e1435d8c2d7b26ed658fc3bb381
vocus <vocus07@gmail.com>
2023-10-15 09:27:38 +0200

    aktualizacja wtyczki

--- update-checker.php
+++ update-checker.php
@@ -6,7 +6,7 @@ function check_for_plugin_update($checked_data)
     $plugin_data     = get_plugin_data(plugin_dir_path(__DIR__) . 'sprawdziany-block-users.php');
     $current_version = $plugin_data['Version'];
 
-    $api_response = wp_remote_get('https://raw.githubusercontent.com/vocus07/sprawdziany-block-user/main/update.json?token=GHSAT0AAAAAACI4OQ4RJREOH3Y7PO5UPSY2ZJKSZRA');
+    $api_response = wp_remote_get('https://raw.githubusercontent.com/vocus07/sprawdziany-block-user/main/update.json');
 
     if (!is_wp_error($api_response) && ($api_response['response']['code'] == 200)) {
         $response = json_decode($api_response['body']);
