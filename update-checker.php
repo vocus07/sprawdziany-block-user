042281e4334134b97328695c0cc9456e623c9bbd
vocus <vocus07@gmail.com>
2023-10-15 10:20:28 +0200

    Aktualizacja pliku

--- update-checker.php
+++ update-checker.php
@@ -3,14 +3,15 @@
 function check_for_plugin_update($checked_data)
 {
     include_once ABSPATH . 'wp-admin/includes/plugin.php';
-    $plugin_data     = get_plugin_data(plugin_dir_path(__DIR__) . 'sprawdziany-block-users.php');
+    $plugin_data     = get_plugin_data(plugin_dir_path(__FILE__) . 'sprawdziany-block-users.php');
     $current_version = $plugin_data['Version'];
 
     $api_response = wp_remote_get('https://raw.githubusercontent.com/vocus07/sprawdziany-block-user/main/update.json');
 
     if (!is_wp_error($api_response) && ($api_response['response']['code'] == 200)) {
         $response = json_decode($api_response['body']);
-        if (version_compare($response->version, $current_version, '>')) {
+
+        if (isset($response->version) && version_compare($response->version, $current_version, '>')) {
             $checked_data->response['sprawdziany-block-users/sprawdziany-block-users.php'] = (array)$response;
         }
     }
